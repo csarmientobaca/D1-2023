fecbb84033e59ff23f518fb3b3bf8459fa7fe7b9
Carlos Sarmiento <c.sarmientobaca@gmail.com>
2023-02-06 15:06:50 +0100

    added button component

---
+++ src/components/Button.js
@@ -0,0 +1,8 @@
+function Button(props) {
+    return (
+        <button>{props.name}</button>
+    )
+
+}
+
+export default Button
---
+++ src/components/ImageComponent.js
@@ -0,0 +1,12 @@
+import { React } from "react"
+
+class ImageComponent extends React.Component {
+    render() {
+
+        return <h1></h1>
+    }
+
+
+}
+
+export default ImageComponent
